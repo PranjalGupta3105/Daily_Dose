445117c159e3183330d6a614eed2bfabe18effd2
Pranjal Gupta <=>
2020-05-08 23:27:18 +0530

    q2 solved

--- 6-5-20-q2.py
+++ 6-5-20-q2.py
@@ -1,15 +1,22 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    rup = 0
+# -- This did not work
+#if __name__ == '__main__':
+#    n = int(input())
+#    arr = list(map(int, input().split()))
+#    rup = 0
 
-    for i in range (len(arr)-1):
-        if(arr[i]<arr[i+1]):
-            rup = arr[i]
+#    for i in range (len(arr)-1):
+#        if(arr[i]<arr[i+1]):
+#            rup = arr[i]
             # print(rup)
-        else:
-            if(arr[i]>arr[i+1]):
-                rup = arr[i+1]
+#        else:
+#            if(arr[i]>arr[i+1]):
+#                rup = arr[i+1]
                 # print(rup)
 
-    print(rup)
+#    print(rup)
+
+
+
+
+
+
